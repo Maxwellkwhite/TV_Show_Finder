b46a124c088855a30489e7f8d870e4ecbc24169e
Maxwellkwhite <maxwellkw@comcast.net>
2024-09-16 16:41:04 -0400

    Movies API now working

--- main.py
+++ main.py
@@ -36,6 +36,27 @@ categories_dictionary = {'Action & Adventure': 10759,
                   'War & Politics': 10768, 
                   'Western': 37,}
 
+movies_categories ={
+    'Action': 28,
+    'Adventure': 12,
+    'Animation': 16, 
+    'Comedy': 35, 
+    'Crime': 80, 
+    'Documentary': 99, 
+    'Drama': 18, 
+    'Family': 10751,
+    'Fantasy': 14,
+    'History':36,
+    'Horror':27,
+    'Music':10402,
+    'Mystery': 9648,
+    'Romance':10749,
+    'Science Fiction':878,
+    'TV Movie':10770,
+    'Thriller':53,
+    'War':10752,
+    'Western': 37,}
+
 app = Flask(__name__)
 ckeditor = CKEditor(app)
 Bootstrap5(app)
@@ -95,7 +116,34 @@ class TV_Filters(FlaskForm):
                                              "I Don't Care",], render_kw={'class':'form_class'})
     quality_of_show = SelectField("Quality of Show", choices=["I Don't Care", 
                                                               "Decent or Better", 
-                                                              "Incredible"], render_kw={'class':'form_class'})
+                                                              "Highly Rated"], render_kw={'class':'form_class'})
+    popularity = SelectField("Popularity of Show", choices=["Popular", 
+                                                            "Any Popularity",], render_kw={'class':'form_class'})
+    submit = SubmitField("Find your show")
+
+class Movie_Filters(FlaskForm):
+    category = SelectField(label="Choose Category", choices=['Action',
+                                                             'Adventure',
+                                                             'Animation', 
+                                                             'Comedy', 
+                                                             'Crime', 
+                                                             'Documentary', 
+                                                             'Drama', 
+                                                             'Family',
+                                                             'Fantasy',
+                                                             'History',
+                                                             'Horror',
+                                                             'Music',
+                                                             'Mystery',
+                                                             'Romance',
+                                                             'Science Fiction',
+                                                             'TV Movie',
+                                                             'Thriller',
+                                                             'War',
+                                                             'Western',], render_kw={'class':'form_class'})
+    quality_of_movie = SelectField("Quality of Show", choices=["I Don't Care", 
+                                                              "Decent or Better", 
+                                                              "Highly Rated"], render_kw={'class':'form_class'})
     popularity = SelectField("Popularity of Show", choices=["Popular", 
                                                             "Any Popularity",], render_kw={'class':'form_class'})
     submit = SubmitField("Find your show")
@@ -150,11 +198,11 @@ def find_show():
             quality = 0
         elif quality_of_show == "Decent or Better":
             quality = 5
-        elif quality_of_show == "Incredible":
-            quality = 8.5
+        elif quality_of_show == "Highly Rated":
+            quality = 7.5
         popularity_value = 0
         if popularity == "Popular":
-            popularity_value = 1000
+            popularity_value = 750
         else:
             popularity_value = 0
         param = {
@@ -265,43 +313,29 @@ def find_show():
 
 @app.route('/movie', methods=["GET", "POST"])
 def find_movie():
-    form=TV_Filters()
+    form=Movie_Filters()
     if form.validate_on_submit():
         list = []
-        five_shows = []
+        five_movies = []
         full_details = []
         seasons = []
         episodes = []
         show_id = []
         homepage = []
-        #sets the page limit at 150 to purposefully fail to get actual page total
-        page_to_fail=150
+        #sets the page limit at 501 to purposefully fail to get actual page total
+        page_to_fail=500
         category = form.category.data
-        with_type = form.with_type.data
-        quality_of_show = form.quality_of_show.data
+        quality_of_movie = form.quality_of_movie.data
         popularity = form.popularity.data
         #create blank type to fille the actual value into it
-        type = ""
-        if with_type == "Documentary":
-            type = 0
-        elif with_type == "Miniseries":
-            type = 2
-        elif with_type == "Reality":
-            type = 3
-        elif with_type == "Scripted":
-            type = 4
-        elif with_type == "Talk Show":
-            type = 5
-        elif with_type == "I Don't Care":
-            type = ''
         # choices=["I Don't Care", "At least Decent", "Incredible"]
         quality = 0
-        if quality_of_show == "I Don't Care":
+        if quality_of_movie == "I Don't Care":
             quality = 0
-        elif quality_of_show == "Decent or Better":
+        elif quality_of_movie == "Decent or Better":
             quality = 5
-        elif quality_of_show == "Incredible":
-            quality = 8.5
+        elif quality_of_movie == "Highly Rated":
+            quality = 7.5
         popularity_value = 0
         if popularity == "Popular":
             popularity_value = 1000
@@ -310,27 +344,26 @@ def find_movie():
         param = {
         "include_adult":"true",
         "with_origin_country": ORIGIN_COUNTRY,
-        "with_genres": categories_dictionary[category],
+        "with_genres": movies_categories[category],
         "vote_average.gte": quality,
         "vote_count.gte": VOTE_COUNT_MIN,
-        "page": page_to_fail,
-        "with_type": type,
+        "page": random.randint(1, page_to_fail),
         "vote_count.gte":popularity_value,
         }
         headers = {
             "Authorization": f"Bearer {API_KEY}",
             "accept": "application/json",
         }
-        response = requests.get("https://api.themoviedb.org/3/discover/tv", headers=headers, params=param)
-        data = response.json()
+        response = requests.get("https://api.themoviedb.org/3/discover/movie", headers=headers, params=param)
+        movie_data = response.json()
         #if the results don't produce a value, rerun the API using new API values
-        if data['results'] == []:
+        if movie_data['results'] == []:
             try:
-                max_pages=data['total_pages']-1
+                max_pages=movie_data['total_pages']-1
                 param = {
                 "include_adult":"true",
                 "with_origin_country": ORIGIN_COUNTRY,
-                "with_genres": categories_dictionary[category],
+                "with_genres": movies_categories[category],
                 "vote_average.gte": quality,
                 "vote_count.gte": VOTE_COUNT_MIN,
                 "page": random.randint(1, max_pages),
@@ -341,14 +374,14 @@ def find_movie():
                     "Authorization": f"Bearer {API_KEY}",
                     "accept": "application/json",
                 }
-                response = requests.get("https://api.themoviedb.org/3/discover/tv", headers=headers, params=param)
-                data = response.json()
+                response = requests.get("https://api.themoviedb.org/3/discover/movie", headers=headers, params=param)
+                movie_data = response.json()
             except ValueError:
-                x=data['total_pages']
+                x=movie_data['total_pages']
                 param = {
                 "include_adult":"true",
                 "with_origin_country": ORIGIN_COUNTRY,
-                "with_genres": categories_dictionary[category],
+                "with_genres": movies_categories[category],
                 "vote_average.gte": quality,
                 "vote_count.gte": VOTE_COUNT_MIN,
                 "page": 1,
@@ -359,21 +392,21 @@ def find_movie():
                     "Authorization": f"Bearer {API_KEY}",
                     "accept": "application/json",
                 }
-                response = requests.get("https://api.themoviedb.org/3/discover/tv", headers=headers, params=param)
-                data = response.json()
-            for x in range (data['total_results']):
-                if data['total_results'] <= 19 and data['total_results'] >= 1:
+                response = requests.get("https://api.themoviedb.org/3/discover/movie", headers=headers, params=param)
+                movie_data = response.json()
+            for x in range (movie_data['total_results']):
+                if movie_data['total_results'] <= 19 and movie_data['total_results'] >= 1:
                     # x = data['results'][random.randint(0,data['total_results']-1)]['name']
                     # show = data['results'][random.randint(0,data['total_results']-1)]
-                    show = data['results'][x]
-                    list.append(show['name'])
+                    show = movie_data['results'][x]
+                    list.append(show['title'])
                     full_details.append(show)
-                elif data['total_results'] == 0:
+                elif movie_data['total_results'] == 0:
                     pass
                 else:
                     # x = data['results'][random.randint(0,19)]['name']
-                    show = data['results'][random.randint(0,19)]
-                    list.append(show['name'])
+                    show = movie_data['results'][random.randint(0,19)]
+                    list.append(show['title'])
                     full_details.append(show)
             # myset = sorted(set(list))
             for x in range(5):
@@ -381,36 +414,27 @@ def find_movie():
                     # chosen_show = random.choice(myset)
                     # five_shows.append(chosen_show)
                     # myset.remove(chosen_show)
-                    chosen_show = random.choice(full_details)
-                    while chosen_show in five_shows:
-                        chosen_show = random.choice(full_details)
-                    five_shows.append(chosen_show)
-                    full_details.remove(chosen_show)
+                    chosen_movie = random.choice(full_details)
+                    while chosen_movie in five_movies:
+                        chosen_movie = random.choice(full_details)
+                    five_movies.append(chosen_movie)
+                    full_details.remove(chosen_movie)
                 except IndexError: #used because some may not have 5 data points
                     pass
-        for show in five_shows:
-            response = requests.get(f"https://api.themoviedb.org/3/tv/{show['id']}?language=en-US", headers=headers, params=param)
-            data = response.json()
-            show_season = data['number_of_seasons']
-            seasons.append(show_season)
-            show_episodes = data['number_of_episodes']
-            episodes.append(show_episodes)
-            # id = data['id']
-            # show_id.append(id)
-            homepage_link = data['homepage']
-            homepage.append(homepage_link)
-        return render_template('tv_results.html', 
+        # for show in five_movies:
+        #     response = requests.get(f"https://api.themoviedb.org/3/tv/{show['id']}?language=en-US", headers=headers, params=param)
+        #     movie_data = response.json()
+        #     # id = data['id']
+        #     # show_id.append(id)
+        return render_template('movie_results.html', 
                                category=category, 
-                               five_shows=five_shows, 
-                               data=data, 
+                               five_shows=five_movies, 
+                               data=movie_data, 
                                details=full_details,
                                genres=form.category.data,
-                               quality=quality_of_show,
-                               type=with_type,
+                               quality=five_movies,
                                seasons=seasons,
-                               episodes=episodes,
-                            #    id=id,
-                               homepage=homepage)
+                               episodes=episodes,)
     return render_template("movie_index.html", form=form)
 
 @app.route('/register', methods=["GET", "POST"])
